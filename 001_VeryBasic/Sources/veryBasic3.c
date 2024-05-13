deb7839ba532dad5714cedcd1184203e7f72b2a6
mdsksadi <sadiewu12345@gmail.com>
2024-05-13 19:34:16 +0200

    001_VeryBasic/Sources/veryBasic3.c file added

---
+++ 001_VeryBasic/Sources/veryBasic3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("  *  \n");
+    printf(" *** \n");
+    printf("*****\n\n");
+
+    /* Another way to do */
+
+    printf("  *  \n *** \n*****\n\n");
+
+    return 0;
+}
