2d8d91d58b0142bf9bf4accc942658bff479d485
mdsksadi <sadiewu12345@gmail.com>
2024-05-13 19:20:39 +0200

    001_VeryBasic/Sources/veryBasic2.c file added

---
+++ 001_VeryBasic/Sources/veryBasic2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Full name: MD Shekh Sadi\n");
+    printf("Age: 28\n");
+    printf("Gender: Male\n\n");
+
+    /* Another way to do */
+    printf("Full name: MD Shekh Sadi \nAge:28 \nGender: Male\n\n");
+
+    return 0;
+}
