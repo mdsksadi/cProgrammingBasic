93bb88e31d2ac130d2bb885a1ad14e2e8ba4c0a5
mdsksadi <sadiewu12345@gmail.com>
2024-05-14 11:09:14 +0200

    002_CommentAndFormatSpecification.c file added

---
+++ 002_CommentAndFormatSpecification/Sources/002_CommentAndFormatSpecification.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // Format specification
+    printf("I am %d years old\n", 28);
+    printf("I am %d years old\n", 15);
+    printf("I am %d years old\n", 25);
+
+    return 0;
+}
