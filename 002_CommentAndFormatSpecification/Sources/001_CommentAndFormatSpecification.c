34b28e1e9f048b8341a154f335fb58aa1f8f1e9c
mdsksadi <sadiewu12345@gmail.com>
2024-05-14 10:59:46 +0200

    001_CommentAndFormatSpecification.c file added

---
+++ 002_CommentAndFormatSpecification/Sources/001_CommentAndFormatSpecification.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // This is a single line commnet
+    printf("MD Shekh Sadi\n");
+
+    /*
+    This is a,
+    Comment Block or multi line comment
+    */
+    printf("I am 28 years old\n");
+
+    return 0;
+}
