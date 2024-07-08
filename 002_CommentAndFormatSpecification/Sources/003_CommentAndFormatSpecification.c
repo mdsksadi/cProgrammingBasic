b57fcb594780842b115cd6c80f68fc559c84b7c4
mdsksadi <sadiewu12345@gmail.com>
2024-07-09 00:08:37 +0200

    003_CommentAndFormatSpecification.c file added
    Also modified the cmake file to run 003_CommentAndFormatSpecification.c file

---
+++ 002_CommentAndFormatSpecification/Sources/003_CommentAndFormatSpecification.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	printf("This year I am %d years old and next year I'm going to be %d years old\n", 28,29);	
+	printf("My average grade is: %f\n", 93.7);
+	printf("My average grade is: %.2f\n", 93.7);
+
+
+	return 0;
+}
