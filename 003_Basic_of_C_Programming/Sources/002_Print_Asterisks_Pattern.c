ac7b8d859057bfaa0fcec33d957820728afeb2a2
mdsksadi <sadiewu12345@gmail.com>
2024-07-09 23:53:15 +0200

    002_Print_Asterisks_Pattern.c added
    - CMakeLists.txt file updated

---
+++ 003_Basic_of_C_Programming/Sources/002_Print_Asterisks_Pattern.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("1*********\n");
+	printf("12********\n");
+	printf("123*******\n");
+	printf("1234******\n");
+	printf("12345*****\n\n");
+
+	// Another Approach with just one print function
+	printf("1*********\n12********\n123*******\n1234******\n12345*****\n\n");
+
+	return 0;
+}
