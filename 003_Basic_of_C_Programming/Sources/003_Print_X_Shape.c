d461ec624841309bb362160996ccbf52848fede1
mdsksadi <sadiewu12345@gmail.com>
2024-07-10 00:05:51 +0200

    003_Print_X_Shape.c file added
    CMakeLists.txt file updated

---
+++ 003_Basic_of_C_Programming/Sources/003_Print_X_Shape.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("*     *\n");
+	printf(" *   * \n");
+	printf("  * *  \n");
+	printf("   *   \n");
+	printf("  * *  \n");
+	printf(" *   * \n");
+	printf("*     *\n\n");
+
+	// Another way just using one print function
+	printf("*     *\n *   * \n  * *  \n   *   \n  * *  \n *   * \n*     *\n\n");
+
+	return 0;
+}
